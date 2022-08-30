bda3da8ff6160d03b86d4210dbb7b9f80e53457d
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-08-30 20:29:49 +0200

    firebase integrated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'simple-crm-a965c',
+    appId: '1:1014738643493:web:fcf52b03c7daf6085c2dcc',
+    storageBucket: 'simple-crm-a965c.appspot.com',
+    apiKey: 'AIzaSyAJAZxEqdGwqlMIP1SKXxtABTvkLzq4mas',
+    authDomain: 'simple-crm-a965c.firebaseapp.com',
+    messagingSenderId: '1014738643493',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'simple-crm-a965c',
+    appId: '1:1014738643493:web:fcf52b03c7daf6085c2dcc',
+    storageBucket: 'simple-crm-a965c.appspot.com',
+    apiKey: 'AIzaSyAJAZxEqdGwqlMIP1SKXxtABTvkLzq4mas',
+    authDomain: 'simple-crm-a965c.firebaseapp.com',
+    messagingSenderId: '1014738643493',
+  },
   production: false
 };
 
