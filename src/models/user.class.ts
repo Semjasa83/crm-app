98a08bd8626ceed8fc0d647c13d6d6747c381040
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-09-07 00:49:37 +0200

    email added

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -1,14 +1,17 @@
 export class User {
     firstName: string;
     lastName: string;
+    email: string;
     birthDate: number;
     street: string;
     zipCode: number;
     city: string;
 
+
     constructor(obj?: any) {
         this.firstName = obj ? obj.firstName : ''; //obj ? : '' --> if obj exist else '' 
         this.lastName = obj ? obj.lastName : ''; // obj? then obj.xxx else ''
+        this.email = obj ? obj.email : '';
         this.birthDate = obj ? obj.birthDate : '';
         this.street = obj ? obj.street : '';
         this.zipCode = obj ? obj.zipCode : '';
@@ -19,6 +22,7 @@ export class User {
         return{
         firstName: this.firstName,
         lastName: this.lastName,
+        email: this.email,
         birthDate: this.birthDate,
         street: this.street,
         zipCode: this.zipCode,
