dcd74e940ae67535851a31ac93fef73cef3a22be
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-09-08 01:46:39 +0200

    user class detail works

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { doc, getDoc, onSnapshot } from "@firebase/firestore";
+import { User } from 'src/models/user.class';
+
 
 @Component({
   selector: 'app-user-detail',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+userId: any = '';
+user: User = new User();
+
+  constructor(private route: ActivatedRoute, private firestore: Firestore) { 
+
   }
 
-}
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( paramMap => {
+      this.userId = paramMap.get('id');
+      console.log('GOT ID', this.userId);
+      this.getUser();
+  })
+  }
+
+  async getUser() {
+    const unsub = onSnapshot(doc(this.firestore, "users", this.userId), (u) => {
+      this.user = new User(u.data());
+      console.log("Current data: ", u.data());
+  });
+  }
+
+  }
+
+
