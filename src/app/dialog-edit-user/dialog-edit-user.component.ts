ffe29259b6646930ee3113bf1583c5969f4d102d
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-09-09 23:50:48 +0200

    prepare edit user

--- src/app/dialog-edit-user/dialog-edit-user.component.ts
+++ src/app/dialog-edit-user/dialog-edit-user.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from 'src/models/user.class';
+import { Firestore, collectionData } from '@angular/fire/firestore';
+import { collection, addDoc } from "firebase/firestore";
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-dialog-edit-user',
   templateUrl: './dialog-edit-user.component.html',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogEditUserComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User;
+  loading = false;
+
+  constructor(public dialogRef: MatDialogRef<DialogEditUserComponent>) { }
 
   ngOnInit(): void {
+  }
+
+  saveUser() {
+
   }
 
 }
